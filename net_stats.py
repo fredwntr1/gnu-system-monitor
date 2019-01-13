fbd7a3c71ee9fdb07bc1f40b3bc4778ac7a7a7f7
Frederick <fredwntr1@protonmail.com>
2019-01-13 13:20:37 -0500

    netstat is old and being replaced with ss

--- net_stats.py
+++ net_stats.py
@@ -3,26 +3,24 @@ import netifaces
 
 
 def net_procs():
-    find_network = "netstat -tnp | grep -i stab | awk -F/ '{print $2}' | sort | uniq"
+    find_network = """ss -tnp | grep -i ESTAB | sort | awk -F '"' '$0=$2'"""
     show_network = subprocess.check_output(find_network, shell=True, universal_newlines=True).splitlines()
     return show_network
 
 
 def net_proc_download():
-    find_download = "netstat -tnp | grep -i tcp | awk '{print $2}' | sort | uniq"
+    find_download = "ss -tnp | grep -i ESTAB | sort | awk '{print $2}'"
     show_download = subprocess.check_output(find_download, shell=True, universal_newlines=True).splitlines()
     download = list(map(str, show_download))
     return download
 
 
 def net_proc_upload():
-    find_upload = "netstat -tnp | grep -i tcp | awk '{print $3}' | sort | uniq"
+    find_upload = "ss -tnp | grep -i ESTAB | sort | awk '{print $3}'"
     show_upload = subprocess.check_output(find_upload, shell=True, universal_newlines=True).splitlines()
-  #  show_upload.remove("Send-Q")
     upload = list(map(str, show_upload))
     return upload
 
 
 
 
-#net_proc_download()
