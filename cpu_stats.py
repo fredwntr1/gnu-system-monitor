48ef2c995f15fe5b20b995ebc8ac9cd48748ad32
fredwntr1 <fredwntr1@protonmail.com>
2019-01-15 18:34:31 -0500

    add intel support for fan speed and temperatures

--- cpu_stats.py
+++ cpu_stats.py
@@ -15,37 +15,39 @@ def cpu_load_percentage():
         return show_cpu_load
 
 
-def cpu_fan_rpm():
-    cpu_fan = 'sensors | grep -m 1 fan | cut -c 24-28'
-    pass_cpu_fan = subprocess.check_output(cpu_fan, shell=True, universal_newlines=True).strip()
-    show_cpu_fan = int(pass_cpu_fan)
-    return show_cpu_fan
 
 
-# Figures out which cpu vendor you are using
-def find_cpu():
-    cpu_type = "lscpu | grep 'Model name:' | cut -c 22-24"
-    find_cpu_model = subprocess.check_output(cpu_type, shell=True, universal_newlines=True).strip()
+def cpu_temp():
+    find_cpu = "lscpu | grep 'Model name:' | cut -c 22-24"
+    find_cpu_model = subprocess.check_output(find_cpu, shell=True, universal_newlines=True).strip()
     show_cpu_model = repr(find_cpu_model)
-    return show_cpu_model
+    if show_cpu_model == repr('AMD'):
+        cpu_temp = 'sensors | grep Tdie: | cut -c 16-19'
+        temp = subprocess.check_output(cpu_temp, shell=True, universal_newlines=True).strip()
+        show_temp = repr(temp)
+        if show_temp == repr(temp):
+            return temp
+        else:
+            fx_temp = 'sensors | grep temp1: | cut -c 16-19'
+            temp = subprocess.check_output(fx_temp, shell=True, universal_newlines=True).strip()
+            return temp
+    elif show_cpu_model == repr('Int'):
+        intel_temperatures = "sensors | grep -E 'Core [0-99]' | cut -c 16-19"
+        temp = subprocess.check_output(intel_temperatures, shell=True, universal_newlines=True).splitlines()
+        temp_ints = np.array(temp)
+        temp_update = temp_ints.astype(np.float)
+        np.around(temp_update)
+        return sum(temp_update) / len(temp_update)
 
-
-def amd_temp():
-    amd_ryzen_temp = 'sensors | grep Tdie: | cut -c 16-19'
-    temp = subprocess.check_output(amd_ryzen_temp, shell=True, universal_newlines=True).strip()
-    show_temp = repr(temp)
-    if show_temp == repr(temp):
-        return temp
-    else:
-        amd_fx_temp = 'sensors | grep temp1: | cut -c 16-19'
-        temp = subprocess.check_output(amd_fx_temp, shell=True, universal_newlines=True ).strip()
-        return temp
-
-
-def intel_temp():
-    intel_temperatures = "sensors | grep -E 'Core [0-99]' | cut -c 16-19"
-    temp = subprocess.check_output(intel_temperatures, shell=True, universal_newlines=True).splitlines()
-    temp_ints = np.array(temp)
-    temp_update = temp_ints.astype(np.float)
-    return sum(temp_update) / len(temp_update)
+def cpu_fan():
+    find_cpu = "lscpu | grep 'Model name:' | cut -c 22-24"
+    find_cpu_model = subprocess.check_output(find_cpu, shell=True, universal_newlines=True).strip()
+    show_cpu_model = repr(find_cpu_model)
+    if show_cpu_model == repr('AMD'):
+        cpu_fan = 'sensors | grep -m 1 fan | cut -c 24-28'
+        pass_cpu_fan = subprocess.check_output(cpu_fan, shell=True, universal_newlines=True).strip()
+        show_cpu_fan = int(pass_cpu_fan)
+        return show_cpu_fan
+    elif show_cpu_model == repr('Int'):
+        return 0
 
--- cpu_worker.py
+++ cpu_worker.py
@@ -3,24 +3,15 @@ from PyQt4 import QtCore
 import cpu_stats
 
 
-
-#class CpuWorkerSignal(QtCore.QObject):
- #   worker_signal = QtCore.pyqtSignal(int, float, float)
-
-
-
 class CpuWorker(QtCore.QThread):
     def __init__(self):
         super(CpuWorker, self).__init__()
-     #   self.signal = CpuWorkerSignal()
 
     def run(self):
+        temp = cpu_stats.cpu_temp()
+        cpu_percent = cpu_stats.cpu_load_percentage()
+        fan = cpu_stats.cpu_fan()
         while True:
-            if cpu_stats.find_cpu() == repr('AMD'):
-                cpu_temp = float(cpu_stats.amd_temp())
-            elif cpu_stats.find_cpu() == repr('INT'):
-                cpu_temp = float(cpu_stats.intel_temp())
-            cpu_fan = cpu_stats.cpu_fan_rpm()
-            cpu_percent = cpu_stats.cpu_load_percentage()
-            self.emit(QtCore.SIGNAL("CPU_STATS"), cpu_fan, cpu_percent, cpu_temp)
+            self.emit(QtCore.SIGNAL("CPU_STATS"), cpu_percent, temp, fan)
             time.sleep(1)
+
