627dbc607380b3442cbd6fa1f4e945aa53f33f44
Frederick <fredwntr1@protonmail.com>
2019-01-14 23:24:07 -0500

    fix a few subprocess commands as well as only supporting nvidia currently

--- gpu_worker.py
+++ gpu_worker.py
@@ -1,17 +1,20 @@
 from PyQt4 import QtCore
 import time
-
-
+import subprocess
 
 
 class GpuWorker(QtCore.QThread):
     def __init__(self):
         super(GpuWorker, self).__init__()
 
+
+
     def run(self):
-        import test
+        gpu_type = "glxinfo | grep 'OpenGL vendor string:'"
+        find_gpu_vendor = subprocess.check_output(gpu_type, shell=True, universal_newlines=True).strip()
+        show_gpu_vendor = repr(find_gpu_vendor)
         while True:
-            if test.find_gpu() == repr('OpenGL vendor string: NVIDIA Corporation'):
+            if show_gpu_vendor  == repr('OpenGL vendor string: NVIDIA Corporation'):
                 import nvidia_gpu_stats
                 nvidia_temp = nvidia_gpu_stats.nvidia_temp()
                 nvidia_mem = nvidia_gpu_stats.nvidia_mem()
@@ -19,9 +22,9 @@ class GpuWorker(QtCore.QThread):
                 nvidia_watts = nvidia_gpu_stats.nvidia_voltage()
                 nvidia_fan = nvidia_gpu_stats.nvidia_fan_speed()
                 self.emit(QtCore.SIGNAL("GPU_STATS"), nvidia_temp, nvidia_mem, nvidia_clock, nvidia_watts, nvidia_fan)
-                time.sleep(3)
-            elif test.find_gpu() == repr(' OpenGL vendor string: AMD'):
+                time.sleep(2)
+            elif show_gpu_vendor == repr(' OpenGL vendor string: AMD'):
                 temp = None
-                break
+                time.sleep(2)
 
 
