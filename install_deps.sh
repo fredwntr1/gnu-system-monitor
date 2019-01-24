31b1a8114fa7c67cfea6ed519f0ae7168c6493c5
fredwntr1 <fredwntr1@protonmail.com>
2019-01-24 16:08:51 -0500

    Let people install their own dependencies

--- install_deps.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt install python3-pyqt4 python3-pip lm-sensors net-tools
-pip3 install --upgrade pip
-pip3 install numpy --user
-pip3 install psutil --user
-pip3 install nvidia-ml-py3 --user
