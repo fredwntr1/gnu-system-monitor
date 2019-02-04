0a64fd269e168ed6ad7c3bfa08147f76655c6d2d
fredwntr1 <fredwntr1@protonmail.com>
2019-02-03 23:59:56 -0500

    Only change permissions if using an AMD gpu

--- systemd/amdfc.py
+++ systemd/amdfc.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
-
+import sys
 import os
+import subprocess
 
-os.system("find / -type f -name 'pwm1*' -exec chmod 777 {} \;")
+
+vendor = "glxinfo | grep 'renderer string:' | awk '{print $4}'"
+display_vendor = subprocess.check_output(vendor, shell=True).strip()
+display_vendor = repr(display_vendor)
+change_perms = "find / -type f -name 'pwm1*' -exec chmod 777 {} \;"
+
+if display_vendor == repr('AMD'):
+    subprocess.check_output(change_perms, shell=True)
+else:
+    sys.exit()
--- systemd/amdst.py
+++ systemd/amdst.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
-
+import sys
 import subprocess
 
+
+vendor = "glxinfo | grep 'renderer string:' | awk '{print $4}'"
+display_vendor = subprocess.check_output(vendor, shell=True).strip()
+display_vendor = repr(display_vendor)
 permissions = "chmod -R a+rX /sys/kernel/debug"
-subprocess.check_output(permissions, shell=True)
+
+if display_vendor == repr('AMD'):
+    subprocess.check_output(permissions, shell=True)
+else:
+    sys.exit()
+
